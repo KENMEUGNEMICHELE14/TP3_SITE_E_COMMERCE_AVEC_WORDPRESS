3c9f6ae47bfe18f7169f56abca0ac66da1917e40
PafeDilane <pafedilane@gmail.com>
2025-11-10 10:43:06 +0100

    Modifications

---
+++ footer.php
@@ -0,0 +1,66 @@
+<!-- Footer -->
+<footer class="site-footer">
+    <div class="footer-content">
+        <div class="footer-column">
+            <h4>Service Client</h4>
+            <ul>
+                <li><a href="#">Centre D'aide</a></li>
+                <li><a href="#">Votre compte</a></li>
+                <li><a href="#">Vos Commandes</a></li>
+                <li><a href="#">Comment Payer</a></li>
+                <li><a href="#">Comment Acheter</a></li>
+                <li><a href="#">Contactez Nous</a></li>
+                <li><a href="#">Mentions Légales</a></li>
+            </ul>
+        </div>
+        <div class="footer-column">
+            <h4>Liens Utiles</h4>
+            <ul>
+                <li><a href="../../pages/vêtements/pageCategorieVetement.html">Vêtements</a></li>
+                <li><a href="../../pages/accessoires/accessoires.html">Accessoires</a></li>
+                <li><a href="../../pages/chaussures/chaussures.html">Chaussures</a></li>
+                <li><a href="../../pages/amenagement_de_chambre/amenagement.html">Aménagement de La chambre</a></li>
+                <li><a href="../../pages/alimentation/alimentation.html">Alimentation</a></li>
+                <li><a href="../../pages/couches_et_lingettes/couche_lingette.html">Couches et Lingettes</a></li>
+                <li><a href="../../pages/textile_de_maternité/textile.html">Textile de Maternité</a></li>
+                <li><a href="../../pages/poussettes/pouset.html">Poussettes</a></li>
+            </ul>
+        </div>
+        <div class="footer-column">
+            <h4>A propos</h4>
+            <ul>
+                <li><a href="#">Qui Sommes Nous</a></li>
+                <li><a href="#">Carrière</a></li>
+                <li><a href="#">Conditions Générales</a></li>
+            </ul>
+        </div>
+        
+        <div class="footer-column">
+            <h4>Methode De paiement</h4>
+            <div class="payment-methods">
+                <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/0/04/Visa.svg" alt="Visa" class="footer-payment-logo"></a>
+                <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg" alt="Mastercard" class="footer-payment-logo"></a>
+                <a href="#"><img src="../../assets/images/images_index/téléchargement.png" alt="American Express" class="footer-payment-logo"></a>
+                <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/b/b5/PayPal.svg" alt="PayPal" class="footer-payment-logo"></a>
+                <a href="#"><img src="../../assets/images/images_index/mtn-mobile-money-logo.jpg" alt="Mtn Money" class="footer-payment-logo"></a>
+                <a href="#"><img src="../../assets/images/images_index/orange-money-logo-png_seeklogo-440383.png" alt="orange money" class="footer-payment-logo"></a>
+            </div>
+        </div>
+
+        <div class="footer-column">
+            <h4>Réseaux Sociaux</h4>
+            <ul>
+                <li><a href="#"><img src="../../assets/images/images_index/facebook.jpg" alt="Facebook" class="social-logo"></a></li>
+                <li><a href="#"><img src="../../assets/images/images_index/instagram.jpg" alt="Instagram" class="social-logo"></a></li>
+                <li><a href="#"><img src="../../assets/images/images_index/twitter.png" alt="Twitter" class="social-logo"></a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <h5>&copy; <strong>BébéConfort 2025 - Tous droits réservés</strong></h5>
+    </div>
+</footer>
+
+
+</body>
+</html>
